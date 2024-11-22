e7abd5457bea1aa6026b7976df0a8c74a0f746f1
Shubham Mishra <meshuham@duck.com>
2024-11-22 13:27:32 +0530

    fixed production bugs

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,17 +1,27 @@
 import { NextResponse } from "next/server"
+import bcrypt from 'bcrypt'
 import { PrismaClient } from '@prisma/client'
 
 const client = new PrismaClient();
 
 export const GET = async () => {
-    await client.user.create({
-        data: {
-            email: "cy.shubham.mishra@gmail.com",
-            name: "Shubham",
-            number: "6268883980",
-            password: ""
+    try {
+        await client.user.create({
+            data: {
+                email: "user@mail.com",
+                name: "Shubham",
+                password: await bcrypt.hash("yourpassword", 10),
+                number: "1234567890"
+            }
+        })
+    } catch (error) {
+        if ((error as { code: string }).code === 'P2002') {
+            return NextResponse.json({
+                message: "Email already exists"
+            }, { status: 400 })
         }
-    })
+        throw error
+    }
     return NextResponse.json({
         message: "hi there"
     })
